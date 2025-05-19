815cbb37c808586493e5882fae269d94fa47bab5
Yw4rf <yw4rf17@gmail.com>
2025-05-18 21:49:22 -0300

    mod: some design changes

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
   TITLE: "トビアス・F (Yw4rf)",
-  DESCRIPTION: "Estudiante de Redes Informáticas | Cybersecurity | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
+  DESCRIPTION: "Tobias Gabriel F. | Cybersecurity Enthusiast | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
   EMAIL: "tobifgabriel@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -10,43 +10,43 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Estudiante de Redes Informáticas | Cybersecurity | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
+  DESCRIPTION: "Tobias Gabriel F. | Cybersecurity Enthusiast | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "Estudiante de Redes Informáticas | Cybersecurity | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
+  DESCRIPTION: "Tobias Gabriel F. | Cybersecurity Enthusiast | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "Estudiante de Redes Informáticas | Cybersecurity | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
+    "Tobias Gabriel F. | Cybersecurity Enthusiast | SOC | DFIR | Hands-on Penetration Testing | CTF Player",
 };
 
 export const SOCIALS: Socials = [
   {
     NAME: "Linkedin",
     HREF: "https://linkedin.com/in/Yw4rf",
-    ICON: "fab fa-linkedin-in",
-    COLOR: "#0A66C2",
+    ICON: "",
+    COLOR: "",
   },
   {
     NAME: "GitHub",
     HREF: "https://github.com/Yw4rf",
-    ICON: "fab fa-github",
-    COLOR: "#FF6B6B",
+    ICON: "",
+    COLOR: "",
   },
   {
     NAME: "Telegram",
     HREF: "https://t.me/Yw4rf",
-    ICON: "fab fa-telegram-plane",
-    COLOR: "#0088cc",
+    ICON: "",
+    COLOR: "",
   },
   {
     NAME: "X (Twitter)",
     HREF: "https://twitter.com/Yw4rf",
-    ICON: "fab fa-x-twitter", // o "fab fa-twitter" si no usás la nueva versión
-    COLOR: "#F9C74F",
+    ICON: "",
+    COLOR: "",
   },
 ];
